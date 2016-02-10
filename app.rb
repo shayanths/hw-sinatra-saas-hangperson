db97e5b122b010b6a0a8c76b0471e9141fff4449
shayanths <ssinnarajah@berkeley.edu>
2016-02-10 06:35:20 +0000

    Pushing Part 1 and Part 2

--- app.rb
+++ app.rb
@@ -39,7 +39,17 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    
+    begin
+      output = @game.guess(letter)
+    rescue ArgumentError
+      flash[:message] = "Invalid guess"
+    end
+    
+    if (output == false)
+      flash[:message] = "You have already guessed that letter"
+    end
+      
     redirect '/show'
   end
   
@@ -49,17 +59,22 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    return_value = @game.check_win_or_lose
+    
+    if return_value == :lose
+      redirect '/lose'
+    elsif return_value == :win
+      redirect '/win'
+    else
+      erb :show
+    end
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
     erb :lose # You may change/remove this line
   end
   
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -6,11 +6,83 @@ class HangpersonGame
   # Get a word from remote "random word" service
 
   # def initialize()
+    
   # end
   
   def initialize(word)
     @word = word
+    @guessed = ''
+    @wrong_guesses = ''
   end
+  
+  def word
+    @word
+  end
+  
+  def guesses
+    @guessed
+  end
+
+  def wrong_guesses
+    @wrong_guesses
+  end
+  
+  def letter?(lookAhead)
+    lookAhead =~ /[[:alpha:]]/
+  end
+  
+  def already_guessed?(val)
+    if @guessed.downcase.include? val.downcase or @wrong_guesses.downcase.include? val.downcase
+      return true
+    end
+  end
+  
+  def guess (val)
+    if !letter?(val)
+      raise ArgumentError, 'Letter is not valid'
+    end
+    
+    if already_guessed?(val) == true
+      return false
+    end
+    
+    if @word.include? val
+      @guessed.concat(val)
+    else
+      @wrong_guesses.concat(val)
+    end
+  end
+  
+  def word_with_guesses
+    final_string = ''
+    @word.split("").each do |i|
+      if @guessed.include? i
+        final_string.concat(i)
+      else
+        final_string.concat("-")
+      end
+    end
+    return final_string
+  end
+  
+  def check_win_or_lose
+    final_string  = word_with_guesses
+    
+    if @wrong_guesses.length >= 7
+      return :lose
+    end
+    
+    if final_string == @word
+      return :win
+    else
+      return :play
+    end
+  
+  end
+  
+  
+    
+  
 
   def self.get_random_word
     require 'uri'
